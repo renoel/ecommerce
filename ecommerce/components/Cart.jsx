9fc35fed5a17b2a8da5e88f168de6bcb8f8806f2
renoel <renoel_e@live.nl>
2022-09-30 18:21:17 +0200

    Cart WIP

--- ecommerce/components/Cart.jsx
+++ ecommerce/components/Cart.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useRef } from 'react';
+import Link from 'next/link';
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai';
+import { TiDeleteOutline } from 'react-icons/ti';
+import toast from 'react-hot-toast';
 
-export default function Cart() {
-  return (
-    <div>Cart</div>
+import { useStateContext } from '../context/StateContext';
+import { urlFor } from '../lib/client';
+
+const Cart = () => {
+  const cartRef = useRef();
+  const {totalPrice, totalQuantities, cartItems, setShowCart} = useStateContext();
+
+  return(
+    <div className='cart-wrapper' ref={cartRef}>
+      <div className='cart-container'>
+        <button type='button'
+        className='cart-heading'
+        onClick={() => setShowCart(false) }>
+          <AiOutlineLeft />
+          <span className="heading">Your Cart</span>
+          <span className='cart-num-items'>({totalQuantities} items)</span>
+        </button>
+
+        {cartItems.length < 1 && (
+          <div className='empty-cart'>
+            <AiOutlineShopping size={150}/>
+            <h3>Your shopping bg is empty </h3>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
+
+export default Cart;
